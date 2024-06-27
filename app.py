bdda59279ae7f5ed1ced50b6f67953cdd976d707
maru12151 <s1f102401168@iniad.org>
2024-06-27 15:47:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ def top():
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    kinoko_count = 3
+    takenoko_count = 5
+    messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
