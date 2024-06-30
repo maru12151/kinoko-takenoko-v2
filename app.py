695374431f82ac982f2f2a747af53eea47235963
maru12151 <s1f102401168@iniad.org>
2024-06-30 21:25:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ messages = ['Kinoko is wonrderful!', 'Takenoko is awesome!']
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
-
+import re
 @app.route('/vote', methods=['POST'])
 def answer():
     global kinoko_count, takenoko_count, messages
@@ -22,6 +22,17 @@ def answer():
 
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+
+    message_html = ''
+    for i in range(len(messages)):
+        message = messages[i]
+        message = re.sub(r'&', r'&amp;', message)
+        message = re.sub(r'<', r'&lt;', message)
+        message = re.sub(r'>', r'&gt;', message)
+        message = re.sub(r'\*(.+)\*', r'<strong>\1</strong>', message)
+        message = re.sub(r'(\d{2,3})-\d+-\d+', r'\1-****-****', message)
+        message_html += '<div class="alert {1}" role="alert">{0}</div>\n'.format(
+            message, 'alert-warning ms-5' if i % 2 == 0 else 'alert-success me-5')
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
